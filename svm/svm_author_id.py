8b411608c2f589614d0a86a1e28c45af6bce3034
benktesh <benktesh.sharma@sony.com>
2020-06-07 18:09:13 -0500

    Refactor code to create function and use optimal C

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -13,7 +13,21 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 import pprint
+from sklearn.metrics import accuracy_score
 
+
+def createAndPredict(c, features_train, labels_train, features_test, labels_test):
+    clf = SVC(kernel="rbf", C = c)
+
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    trainingTime = round(time()-t0, 3),
+
+    t0 = time()
+    pred = clf.predict(features_test)
+    testingTime = round(time()-t0, 3)
+    accuracy = accuracy_score(pred, labels_test)
+    return pred, testingTime, trainingTime, accuracy
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -21,8 +35,8 @@ import pprint
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+features_train_subset = features_train[:len(features_train)/100]
+labels_train_subset = labels_train[:len(labels_train)/100]
 
 #########################################################
 ### your code goes here ###
@@ -31,27 +45,33 @@ from sklearn.svm import SVC
 result = []
 CS = [10, 100, 1000, 10000]
 
-print 'Testing four C values'
+
+#CS = [10000]
+selectedC = CS[0]
+maxAcc = -1
+print 'Testing %d values to get optimal value for c on subset', len(CS) 
 for c in CS:    
-    clf = SVC(kernel="rbf", C = c)
-
-    t0 = time()
-    clf.fit(features_train, labels_train)
-    print "Training time:", round(time()-t0, 3), "s"
 
 
-
-    t0 = time()
-    pred = clf.predict(features_test)
-    print 'Testing time:', round(time()-t0, 3), "s"
-
-    #########################################################
-    from sklearn.metrics import accuracy_score
-    acc = accuracy_score(pred, labels_test)
-    print "Accuracy from SVM: "
-    print(acc)
-
-    result.append({"C": c, "accuracy": acc})
+    pred, testingTime, trainingTime, acc = createAndPredict(c, features_train_subset, labels_train_subset, features_test, labels_test)
+    
+    if(acc > maxAcc):
+        selectedC = c
+        maxAc = acc
+    result.append({"C": c, "accuracy": acc, "testingTime":testingTime, "trainingTime": trainingTime })
 
 pprint.pprint(result)
 print '............Done testing four C values'
+
+#final result
+print "Running final with C: ", selectedC , "full data"
+pred, testingTime, trainingTime, acc = createAndPredict(c, features_train, labels_train, features_test, labels_test)
+
+print 'Testing Time', testingTime, "s."
+print 'Training Time', trainingTime, "s." 
+print 'Accuracy: ', acc * 100 , "%."
+
+
+
+
+
