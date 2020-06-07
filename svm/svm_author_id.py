5127957b67a8ae5e9e4520f93c6d39ade11f1a72
benktesh <benktesh.sharma@sony.com>
2020-06-07 18:45:52 -0500

    Refactor code to allow optiizing, using subset and certain feature on fly.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -14,6 +14,7 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 import pprint
 from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
 
 def createAndPredict(c, features_train, labels_train, features_test, labels_test):
@@ -29,47 +30,59 @@ def createAndPredict(c, features_train, labels_train, features_test, labels_test
     accuracy = accuracy_score(pred, labels_test)
     return pred, testingTime, trainingTime, accuracy
 
+def runMuiltipleCs(CS, features_train_subset, labels_train_subset,features_test,labels_test ):
+    print 'Testing %d values to get optimal value for c on subset', len(CS) 
+    selectedC = CS[0]
+    maxAcc = -1
+    for c in CS:    
+        pred, testingTime, trainingTime, acc = createAndPredict(c, features_train_subset, labels_train_subset, features_test, labels_test)
+        if(acc > maxAcc):
+            selectedC = c
+            maxAc = acc
+        result.append({"C": c, "accuracy": acc, "testingTime":testingTime, "trainingTime": trainingTime })
+    return result, selectedD, maxAcc 
+
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+#by default C=10000
+selectedC = 10000 
+optimizeC = False
+useSubset = False
+
+
 features_train_subset = features_train[:len(features_train)/100]
 labels_train_subset = labels_train[:len(labels_train)/100]
 
-#########################################################
-### your code goes here ###
-from sklearn.svm import SVC
-
-result = []
-CS = [10, 100, 1000, 10000]
+if(optimizeC):
+    result, selC, maxAcc = runMuiltipleCs(CS, features_train_subset, labels_train_subset, features_test, labels_test)
+    pprint.pprint(result)
+    selectedC = selC
 
 
-#CS = [10000]
-selectedC = CS[0]
-maxAcc = -1
-print 'Testing %d values to get optimal value for c on subset', len(CS) 
-for c in CS:    
+if(not useSubset):
+    pred, testingTime, trainingTime, acc = createAndPredict(selectedC, features_train, labels_train, features_test, labels_test)
+else: 
+    pred, testingTime, trainingTime, acc = createAndPredict(selectedC, features_train_subset, labels_train_subset, features_test, labels_test)
 
-
-    pred, testingTime, trainingTime, acc = createAndPredict(c, features_train_subset, labels_train_subset, features_test, labels_test)
-    
-    if(acc > maxAcc):
-        selectedC = c
-        maxAc = acc
-    result.append({"C": c, "accuracy": acc, "testingTime":testingTime, "trainingTime": trainingTime })
-
-pprint.pprint(result)
-print '............Done testing four C values'
-
-#final result
-print "Running final with C: ", selectedC , "full data"
-pred, testingTime, trainingTime, acc = createAndPredict(c, features_train, labels_train, features_test, labels_test)
+print "Value 1: ", len(pred[pred==1])
+print "Value 0: ", len(pred[pred==0])
 
 print 'Testing Time', testingTime, "s."
 print 'Training Time', trainingTime, "s." 
 print 'Accuracy: ', acc * 100 , "%."
+
+print "Prediction for 10, 26 50: ",  pred[[10, 26, 50]]
+
+
+
+
+
+
+
 
 
 
