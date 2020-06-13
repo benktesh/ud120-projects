462e4e5d0f165e5c0f68116b5007cc2ef5223e81
benktesh <benktesh.sharma@sony.com>
2020-06-12 20:38:33 -0500

    Implements decision tree.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -11,21 +11,30 @@
 import sys
 from time import time
 sys.path.append("../tools/")
+sys.path.append("../choose_your_own/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier 
+import pprint
 
+from class_vis import prettyPicture, output_image
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab as pl
+
+
+def classify(features_train, labels_train):
+    clf = DecisionTreeClassifier(min_samples_split=40)
+    clf = clf.fit(features_train, labels_train)
+    return clf
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+print(len(features_train[0]))
+clf = classify(features_train, labels_train)
+clf.predict(features_test)
+acc = clf.score(features_test, labels_test)
 
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+print(acc)
