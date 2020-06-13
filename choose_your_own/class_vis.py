bad27bc673523398f09a2295e4bed385f63fc51d
benktesh <benktesh.sharma@sony.com>
2020-06-13 07:08:25 -0500

    Creates another picture

--- choose_your_own/class_vis.py
+++ choose_your_own/class_vis.py
@@ -35,6 +35,39 @@ def prettyPicture(clf, X_test, y_test):
 
     plt.savefig("test.png")
 
+
+def prettyPicture(clf, X_test, y_test, x_label="xLabel", y_label="yLabel", file_name="test.png"):
+    x_min = 0.0; x_max = 1.0
+    y_min = 0.0; y_max = 1.0
+    
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    h = .01  # step size in the mesh
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    plt.pcolormesh(xx, yy, Z, cmap=pl.cm.seismic)
+
+    # Plot also the test points
+    grade_sig = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    bumpy_sig = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==0]
+    grade_bkg = [X_test[ii][0] for ii in range(0, len(X_test)) if y_test[ii]==1]
+    bumpy_bkg = [X_test[ii][1] for ii in range(0, len(X_test)) if y_test[ii]==1]
+
+    plt.scatter(grade_sig, bumpy_sig, color = "b", label="fast")
+    plt.scatter(grade_bkg, bumpy_bkg, color = "r", label="slow")
+    plt.legend()
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    plt.savefig(file_name)
+
+
 import base64
 import json
 import subprocess
