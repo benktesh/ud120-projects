6b4c843a36ce028bc032bcf6f0b4ac625af722da
benktesh <benktesh.sharma@sony.com>
2020-06-20 16:10:52 -0500

    Cleans signature words

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -44,28 +44,39 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
+            print (path)
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
 
+            keys = ["sara", "shackleton", "chris", "germani"]
+            for k in keys:
+                words = words.replace(k, "")
+
             ### append the text to word_data
+
+            word_data.append(words)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
 
+            from_data.append(0 if name == 'sara' else 1)
 
             email.close()
 
-print "emails processed"
+print ("emails processed")
 from_sara.close()
 from_chris.close()
+
 
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
+
+print ('Word data :', word_data[152])
 
 
 
